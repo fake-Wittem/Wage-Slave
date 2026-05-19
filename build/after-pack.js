d0361710334d9f6185bec874a1b02e47b262531e
patrick_cap <2913452246@qq.com>
2026-05-19 18:18:19 +0800

    fix: set Windows executable metadata

--- build/after-pack.js
+++ build/after-pack.js
@@ -10,8 +10,22 @@ exports.default = async function afterPack(context) {
   const exePath = path.join(context.appOutDir, `${context.packager.appInfo.productFilename}.exe`);
   const iconPath = path.join(projectDir, "src", "assets", "app-icon.ico");
   const rceditPath = path.join(projectDir, "node_modules", "electron-winstaller", "vendor", "rcedit.exe");
+  const appName = context.packager.appInfo.productName;
+  const version = context.packager.appInfo.version;
 
-  execFileSync(rceditPath, [exePath, "--set-icon", iconPath], {
-    stdio: "inherit"
-  });
+  execFileSync(
+    rceditPath,
+    [
+      exePath,
+      "--set-icon", iconPath,
+      "--set-version-string", "CompanyName", "fake-Wittem",
+      "--set-version-string", "FileDescription", appName,
+      "--set-version-string", "ProductName", appName,
+      "--set-version-string", "InternalName", appName,
+      "--set-version-string", "OriginalFilename", `${appName}.exe`,
+      "--set-file-version", version,
+      "--set-product-version", version
+    ],
+    { stdio: "inherit" }
+  );
 };
